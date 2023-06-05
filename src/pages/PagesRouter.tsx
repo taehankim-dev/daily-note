2f479e2d69859a19f4f57345e890c2a65853c058
TaeHan <goldmini95@gmail.com>
2023-06-05 11:47:01 +0900

    firebase 설치, 셋팅

--- src/pages/PagesRouter.tsx
+++ src/pages/PagesRouter.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import DiaryList from './diary/DiaryList';
+import SignIn from './sign/SignIn';
+import SignUp from './sign/SignUp';
 
 const PagesRouters : React.FC = () => {
   return (
     <Routes>
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/diarylist" element={<DiaryList />} />
     </Routes>
   )
